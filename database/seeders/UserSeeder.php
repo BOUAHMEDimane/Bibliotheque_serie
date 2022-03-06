7e90b1629a1dab4e2b7353413030b2daa53103a8
bouahmedImane <imanebouahmed@gmail.com>
2022-03-06 22:52:21 +0100

    add user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'id' => '12',
+            'name' => 'bouahmed', 
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => '000',
+            'password' => bcrypt('123456')
+        ]);
+        
+    }
+}
