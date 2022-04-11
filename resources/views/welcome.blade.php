1421e80c3c1b04360d4dbc94433a37bb54be65aa
bouahmedImane <imanebouahmed@gmail.com>
2022-04-11 22:41:00 +0200

    ajouter des étoiles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,11 +24,16 @@
                     <img class="thumbnail" src="../media/images/CCC.jpg" width="100%">
                     <p><strong>{{ $serie->content }}</strong></p>
                     <div class="callout">
-                        <ul class="menu simple">
-                            <li><a href="#">Author: {{ $serie->name }}</a></li>
-                            <li><a href="#">Comments: </a></li>
+                        <ul class="menu simple" aligne="center" >
+                            <li>Author: {{ $serie->name }}</li>
+                            <li>Note :  <i class="fa fa-star fa-1x"  data-index="1"></i> </li>
+                            <li><i class="fa fa-star fa-1x"  data-index="2"></i> </li>
+                            <li><i class="fa fa-star fa-1x"  data-index="3"></i> </li>
+                            <li><i class="fa fa-star fa-1x"  data-index="4"></i> </li>
+                            <li><i class="fa fa-star fa-1x"  data-index="5"></i> </li>
                         </ul>
                     </div>
+                    
                 </div>
             </div>
         </div>
@@ -37,3 +42,88 @@
 
 @endsection
 
+ <!-- Annotation systéme étoile avec jQuery et ajax -->
+
+ <?php
+
+ if (isset($_POST['save'])){
+     $conn = new sqlite('localhost', '', '', 'DB1');
+
+     $uID = $conn->real_escape_string($_POST['uID']);
+     $ratedIndex = $conn->real_escape_string($_POST['ratedIndex']);
+     $ratedIndex++;
+
+     if(!$uID){
+         $conn->query("INSERT INTO stars (rateIndex) VALUE ('$ratedIndex')");
+         $sql = $conn->query("SELECT id FROM stars ORDER BY id desc LIMIT 1");
+         $uData = $sql->fetch_assoc();
+         $uID = $uData['id'];
+     }else
+        $conn->query("UPDATE stars SETrateIndex= '$ratedIndex' WHERE id='$uID'");
+
+    exit(json_encode(array('id'=>$uID)));
+     
+ }
+
+?>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="  crossorigin="anonymous"></script>
+<script>
+    var ratedIndex = 0, uID = 0;
+    $(document).ready(function (){
+        resetStarColor();
+
+        if(localStorage.getItem('ratedIndex') != null){
+            setStars(parseInt(localStorage.getItem('ratedIndex')));
+            uID = localStorage.getItem('uID');
+        };
+
+        $('.fa-star').on('click', function(){
+            ratedIndex= parseInt($(this).data('index'));
+            localStorage.setItem('ratedIndex',ratedIndex);
+            //saveToTheDB();
+        });
+
+        $('.fa-star').mouseover(function () {
+            resetStarColor();
+            var currentIndex = parseInt($(this).data('index'));
+            setStars(currentIndex);
+            
+        });
+
+        $('.fa-star').mouseleave(function () {
+            resetStarColor();
+
+            if(ratedIndex != 0)
+            setStars(ratedIndex);
+               
+        });
+    });
+
+    function saveToTheDB () {
+        $.ajax({
+            url:"welcome.blade.php",
+            methode: "POST",
+            dataType:'json',
+            data:{
+                save: 1,
+                uID: uID,
+                ratedIndex: ratedIndex,
+            }, success: function(r) {
+                uID = r.uid;localStorage.setItem('uID', uID);
+            }
+        });
+
+    }    
+    function setStars (max) {
+            for (var i = 0; i < max; i++)
+                $('.fa-star:eq('+i+')').css('color','yellow');
+
+        }
+
+        function resetStarColor() {
+        $('.fa-star').css('color', 'gray');
+    }
+
+    
+</script>
